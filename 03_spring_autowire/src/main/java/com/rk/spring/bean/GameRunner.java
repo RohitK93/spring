26781b825502a17d67d5adff29485b592480c172
Rohit Kadam <rohit.kadam@Mac.lan>
2025-10-24 18:34:38 +0530

    added spring autowire

--- 03_spring_autowire/src/main/java/com/rk/spring/bean/GameRunner.java
+++ 03_spring_autowire/src/main/java/com/rk/spring/bean/GameRunner.java
@@ -6,12 +6,18 @@ import org.springframework.stereotype.Component;
 @Component
 public class GameRunner {
 	
-	@Autowired
+	/*
+	 * 
+	 * @Autowired private GamingConsole game;
+	 */
+	
+	
 	private GamingConsole game;
 	
-	/*
-	 * public GameRunner(GamingConsole game) { this.game = game; }
-	 */
+	@Autowired
+	public GameRunner(GamingConsole game) { 
+		this.game = game; 
+	}
 
 	public void run() {
 		game.up();
